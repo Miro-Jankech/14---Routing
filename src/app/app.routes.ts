cc0557e9931d7e674e85a60e9effd7ec23cfe91a
Miro-Jankech <jankechmiro@gmail.com>
2025-09-24 16:32:33 +0200

    redirecting users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { Component } from "@angular/core";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./header/not-found/not-found.component";
 
 export const routes : Routes = [
   {
@@ -15,6 +16,11 @@ export const routes : Routes = [
     component: UserTasksComponent,
     children : [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix'
+      },
+      {
         path: 'tasks',
         component: TasksComponent,
       },
@@ -23,5 +29,9 @@ export const routes : Routes = [
         component: NewTaskComponent
       }
     ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ]  
